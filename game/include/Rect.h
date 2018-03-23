41ebe7dff33c0a0a4d0b5a8bbfbc0534e645dd42
EdgarFabiano <eddy.led@gmail.com>
2018-03-22 22:22:09 -0300

    Rect + Vect2

--- game/include/Rect.h
+++ game/include/Rect.h
@@ -6,6 +6,8 @@
 #define RECT_CLASS
 
 
+#include "Vec2.h"
+
 class Rect {
 public:
     float x, y, w, h;
@@ -14,6 +16,15 @@ public:
 
     Rect(float x, float y, float w, float h);
 
+    Rect SumVec2(Vec2 v);
+
+    Rect SubVec2(Vec2 v);
+
+    Vec2 CenterCoord();
+
+    float DistRecs(Rect r);
+
+    bool Contains(Vec2 v);
 };
 
 
--- game/include/Vec2.h
+++ game/include/Vec2.h
@@ -5,12 +5,41 @@
 #ifndef VEC2_CLASS
 #define VEC2_CLASS
 
+#include <math.h>
+
 
 class Vec2 {
 public:
     float x, y;
+
     Vec2();
+
     Vec2(float x, float y);
+
+    Vec2 Sum(Vec2 v);
+
+    Vec2 Sub(Vec2 v);
+
+    Vec2 Mul(float escalar);
+
+    float Mag();
+
+    Vec2 Norm();
+
+    float Dist(Vec2 v);
+
+    float InclX();
+
+    float Incl(Vec2 v);
+
+    Vec2 Rotate(float ang);
+
+    Vec2& operator+(const Vec2& v);
+
+    Vec2& operator-(const Vec2& v);
+
+    Vec2& operator=(const Vec2& v);
+
 };
 
 
