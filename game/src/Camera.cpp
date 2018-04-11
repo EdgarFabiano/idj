be69a08f9a5370a456268f10f101abee4be33173
EdgarFabiano <eddy.led@gmail.com>
2018-04-11 19:37:42 -0300

    Lidando com os casos de direcionais na diagonal

--- game/src/Camera.cpp
+++ game/src/Camera.cpp
@@ -27,14 +27,29 @@ void Camera::Update(float dt) {
         speed = {0, 0};
         Vec2 atual = {CAMERA_SPEED * dt, 0};
 
-        if (inputManager.IsKeyDown(RIGHT_ARROW_KEY)) {
+        bool right = inputManager.IsKeyDown(RIGHT_ARROW_KEY);
+        bool down = inputManager.IsKeyDown(DOWN_ARROW_KEY);
+        bool left = inputManager.IsKeyDown(LEFT_ARROW_KEY);
+        bool up = inputManager.IsKeyDown(UP_ARROW_KEY);
+        
+        if (right) {
             speed = atual;
-        } else if (inputManager.IsKeyDown(DOWN_ARROW_KEY)) {
+        } else if (down) {
             speed = atual.Rotate((float)M_PI / 2);
-        } else if (inputManager.IsKeyDown(LEFT_ARROW_KEY)) {
+        } else if (left) {
             speed = atual.Rotate((float)M_PI);
-        } else if (inputManager.IsKeyDown(UP_ARROW_KEY)) {
+        } else if (up) {
             speed = atual.Rotate((float)-M_PI / 2);
+        }
+
+        if(right && down){
+            speed = atual.Rotate((float) 0.79);
+        } else if(left && down){
+            speed = atual.Rotate((float) 2.36);
+        } else if(left && up){
+            speed = atual.Rotate((float) 3.93);
+        } else if(right && up){
+            speed = atual.Rotate((float) 5.5);
         }
         pos += speed;
     }
